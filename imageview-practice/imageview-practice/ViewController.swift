71ac4ddbd690fbfea0a6dde062215f2092f7cae1
Changyeon-Seo <scy7351@naver.com>
2020-02-01 23:42:16 +0900

    inital commit (2장 이미지뷰 기반 프로그램 직접 만들기)

---
+++ imageview-practice/imageview-practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  imageview-practice
+//
+//  Created by MOZZET on 2020/01/31.
+//  Copyright © 2020 skfo763. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
