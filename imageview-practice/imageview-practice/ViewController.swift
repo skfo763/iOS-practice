c08fcbfc4f0ec8dbd9246c5525a02bc0c68ef626
Changyeon-Seo <scy7351@naver.com>
2020-02-02 00:50:19 +0900

    [2장] 이미지 뷰 활용하기 - 교재에 나오는 예제 코드

--- imageview-practice/imageview-practice/ViewController.swift
+++ imageview-practice/imageview-practice/ViewController.swift
@@ -9,12 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var isZoom = false
+    var imageOn: UIImage?
+    var imageOff: UIImage?
 
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var resizeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imageOn = UIImage(named: "lamp_on.png")
+        imageOff = UIImage(named: "lamp_off.png")
+        imageView.image = imageOn
     }
 
-
+    @IBAction func resizeImage(_ sender: UIButton) {
+        let scale: CGFloat = 2.0
+        var newWidth: CGFloat, newHeight: CGFloat
+        
+        if(isZoom) {
+            // 이미지가 이미 확대되어 있다면 -> 이미지를 축소합니다.
+            newWidth = imageView.frame.width / scale
+            newHeight = imageView.frame.height / scale
+            
+            // 버튼의 텍스트를 'expand'로 바꿉니다.
+            resizeButton.setTitle("expand", for: .normal)
+        } else {
+            // 이미지가 축소된 상태라면 -> 이미지를 확대합니다.
+            newWidth = imageView.frame.width * scale
+            newHeight = imageView.frame.height * scale
+            
+            // 버튼의 텍스트를 'reduce'로 바꿉니다.
+            resizeButton.setTitle("reduce", for: .normal)
+        }
+        imageView.frame.size = CGSize(width: newWidth, height: newHeight)
+        isZoom = !isZoom
+    }
+    
+    @IBAction func switchImateState(_ sender: UISwitch) {
+        if sender.isOn {
+            imageView.image = imageOn
+        } else {
+            imageView.image = imageOff
+        }
+    }
 }
 
